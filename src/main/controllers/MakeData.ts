8fc00905255c7792f51fe6edd3ce58b1209e9e8e
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-08-07 23:37:12 -0300

    feat: return erro 500

--- src/main/controllers/MakeData.ts
+++ src/main/controllers/MakeData.ts
@@ -11,55 +11,60 @@ interface Params {
 
 export class MakeData {
   async handle(req: Request, res: Response) {
-    const { pageName, dateStart, dateEnd } = req.query as unknown as Params;
+    try {
+      const { pageName, dateStart, dateEnd } = req.query as unknown as Params;
 
-    if (!pageName || !dateStart || !dateEnd) {
-      return res.status(400).send('Missing params');
-    }
-
-    const spreadsheet = new Spreadsheet(
-      process.env.PRIVATE_KEY as string,
-      process.env.CLIENT_EMAIL as string,
-      process.env.SPREAD_SHEET_ID as string
-    );
-
-    const rows = await spreadsheet.getRows(pageName);
-    const filteredRows = rows.filter((row) => {
-      const date = formatDate(row.discoveryDate);
-      return date >= new Date(dateStart) && date <= new Date(dateEnd);
-    });
-
-    const typeStats = new Map<Type, RowAdapter[]>();
-
-    filteredRows.forEach((row) => {
-      const type = row.type;
-      if (typeStats.has(type)) {
-        typeStats.get(type)?.push(row);
-      } else {
-        typeStats.set(type, [row]);
+      if (!pageName || !dateStart || !dateEnd) {
+        return res.status(400).send('Missing params');
       }
-    });
 
-    const itens = new Map<Type, unknown>();
+      const spreadsheet = new Spreadsheet(
+        process.env.PRIVATE_KEY as string,
+        process.env.CLIENT_EMAIL as string,
+        process.env.SPREAD_SHEET_ID as string
+      );
 
-    typeStats.forEach((type, key) => {
-      const stateStats = {
-        total: type.length
-      };
-      type.forEach(({ state }) => {
-        if (stateStats[state]) {
-          const count = stateStats[state];
-          stateStats[state] = count + 1;
+      const rows = await spreadsheet.getRows(pageName);
+      const filteredRows = rows.filter((row) => {
+        const date = formatDate(row.discoveryDate);
+        return date >= new Date(dateStart) && date <= new Date(dateEnd);
+      });
+
+      const typeStats = new Map<Type, RowAdapter[]>();
+
+      filteredRows.forEach((row) => {
+        const type = row.type;
+        if (typeStats.has(type)) {
+          typeStats.get(type)?.push(row);
         } else {
-          stateStats[state] = 1;
+          typeStats.set(type, [row]);
         }
       });
 
-      itens.set(key, stateStats);
-    });
+      const itens = new Map<Type, unknown>();
 
-    const result = Object.fromEntries([...itens.entries()]);
+      typeStats.forEach((type, key) => {
+        const stateStats = {
+          total: type.length
+        };
+        type.forEach(({ state }) => {
+          if (stateStats[state]) {
+            const count = stateStats[state];
+            stateStats[state] = count + 1;
+          } else {
+            stateStats[state] = 1;
+          }
+        });
 
-    res.json({ result, total: filteredRows.length });
+        itens.set(key, stateStats);
+      });
+
+      const result = Object.fromEntries([...itens.entries()]);
+
+      return res.json({ result, total: filteredRows.length });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Internal server error');
+    }
   }
 }
